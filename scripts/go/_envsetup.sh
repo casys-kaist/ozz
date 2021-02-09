bb4123fafa6d131179d45c407836ce13ca8b7710
Dae R. Jeong <threeearcat@gmail.com>
2021-02-09 05:05:30 +0000

    scripts/go: _envsetup.sh and _install.sh

---
+++ scripts/go/_envsetup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+# Be aware of conflictions with GO's environment variables
+__export_envvar "GO" "$TOOLCHAINS_DIR/go"
+__append_path "$GO_PATH/bin"
+export GO="$GOROOT/bin/go"
+export GOROOT="$GO_PATH"
+export GOPATH="$GOTOOLS_DIR"
+export GOOS="linux"
+export GOARCH="amd64"
+export GO_VERSION="go1.15.8"
