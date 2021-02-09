b565715d3e46c76eb3d550330ea9e1a4baae8217
Dae R. Jeong <threeearcat@gmail.com>
2021-02-09 13:38:19 +0000

    StoreBufferPass: very first pass

--- tools/StoreBufferPass/lib/SSBPass.cpp
+++ tools/StoreBufferPass/lib/SSBPass.cpp
@@ -1,1 +1,36 @@
 #include "SSBPass.h"
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+PreservedAnalyses SoftStoreBuffer::run(Function &F,
+                                       FunctionAnalysisManager &AM) {
+  errs() << F.getName() << "\n";
+  return PreservedAnalyses::all();
+}
+
+llvm::PassPluginLibraryInfo getSoftStoreBufferPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "SoftStoreBuffer", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "ssb") {
+                    FPM.addPass(SoftStoreBuffer());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize SoftStoreBuffer when added to the pass pipeline on the
+// command line, i.e. via '-passes=hello-world'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getSoftStoreBufferPluginInfo();
+}
