9427dc1d3170275fc5e483ac935ae2a48f99a1f4
Dae R. Jeong <threeearcat@gmail.com>
2021-02-10 06:31:57 +0000

    StoreBufferPass: Add legacy PassManager stuffs
    I want to run the pass automatically when the pass is loaded, but the
    new PassManager documentation lacks of how to do it. Just use the
    legacy one for now

--- tools/StoreBufferPass/lib/SSBPass.cpp
+++ tools/StoreBufferPass/lib/SSBPass.cpp
@@ -4,11 +4,55 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "llvm/CodeGen/RegAllocRegistry.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 using namespace llvm;
 
+namespace {
+
+/*
+ *Pass Implementation
+ */
+static void visitor(Function &F) { errs() << F.getName() << "\n"; }
+
+/*
+ * Legacy PassManager stuffs
+ */
+struct LegacySoftStoreBuffer : public FunctionPass {
+  static char ID;
+  LegacySoftStoreBuffer() : FunctionPass(ID) {}
+  // Main entry point - the name conveys what unit of IR this is to be run on.
+  bool runOnFunction(Function &F) override {
+    visitor(F);
+    // Doesn't modify the input unit of IR, hence 'false'
+    return false;
+  }
+};
+
+char LegacySoftStoreBuffer::ID = 0;
+
+static RegisterPass<LegacySoftStoreBuffer>
+    X("ssb", "SoftStoreBuffer Pass",
+      true, // This pass doesn't modify the CFG => true
+      false // This pass is not a pure analysis pass => false
+    );
+
+static llvm::RegisterStandardPasses
+    Y(llvm::PassManagerBuilder::EP_EarlyAsPossible,
+      [](const llvm::PassManagerBuilder &Builder,
+         llvm::legacy::PassManagerBase &PM) {
+        PM.add(new LegacySoftStoreBuffer());
+      });
+} // namespace
+
+/*
+ * New PassManager stuffs
+ */
 PreservedAnalyses SoftStoreBuffer::run(Function &F,
                                        FunctionAnalysisManager &AM) {
-  errs() << F.getName() << "\n";
+  visitor(F);
   return PreservedAnalyses::all();
 }
 
@@ -27,9 +71,6 @@ llvm::PassPluginLibraryInfo getSoftStoreBufferPluginInfo() {
           }};
 }
 
-// This is the core interface for pass plugins. It guarantees that 'opt' will
-// be able to recognize SoftStoreBuffer when added to the pass pipeline on the
-// command line, i.e. via '-passes=hello-world'
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
   return getSoftStoreBufferPluginInfo();
