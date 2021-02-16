bc91b580385094076f4765c6dfe12facc80ea12b
Dae R. Jeong <threeearcat@gmail.com>
2021-02-16 08:46:22 +0000

    benchmarks/tso-simple: fix a few things
    1) Fix attributes
    2) Explicity convert void* into struct shared_t *

--- tools/benchmarks/tso-simple/tso-simple.c
+++ tools/benchmarks/tso-simple/tso-simple.c
@@ -1,4 +1,6 @@
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 
 #include <pthread.h>
 #include <stdbool.h>
@@ -10,24 +12,25 @@ struct shared_t {
 	int b;
 };
 
-__attribute__((storebuffer))
+__attribute__((softstorebuffer))
 void *thr1(void *_arg)
 {
-	struct shared_t *arg = _arg;
+	struct shared_t *arg = (struct shared_t *)_arg;
 	arg->b = 1;
 	int la = arg->a;
 	return (void *)(intptr_t)la;
 }
 
-__attribute__((storebuffer))
+__attribute__((softstorebuffer))
 void *thr2(void *_arg)
 {
-	struct shared_t *arg = _arg;
+	struct shared_t *arg = (struct shared_t *)_arg;
 	arg->a = 1;
 	int lb = arg->b;
 	return (void *)(intptr_t)lb;
 }
 
+__attribute__((no_softstorebuffer))
 int main(int argc, char *argv[])
 {
 	pthread_t pth1, pth2;
