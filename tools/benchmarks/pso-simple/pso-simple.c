2f23848ae5a70fbdac185968015df40cb2aeb396
Dae R. Jeong <threeearcat@gmail.com>
2021-02-19 06:39:02 +0000

    benchmarks/pso-simple: minor modificaton of pso-simple
    1) Add sleep() to force thread scheduling
    2) Provide an input for storebuffer

--- tools/benchmarks/pso-simple/pso-simple.c
+++ tools/benchmarks/pso-simple/pso-simple.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
 
 struct shared_t {
 	int *ptr;
@@ -17,6 +19,7 @@ void *writer(void *_arg)
 	struct shared_t *arg = (struct shared_t *)_arg;
 	arg->ptr = (int *)malloc(sizeof(*arg->ptr));
 	arg->ready = true;
+	sleep(3);
 	return NULL;
 }
 
@@ -24,14 +27,26 @@ __attribute__((softstorebuffer))
 void *reader(void *_arg)
 {
 	struct shared_t *arg = (struct shared_t *)_arg;
+	sleep(1);
 	if (arg->ready)
 		(void)*arg->ptr;
 	return NULL;
 }
 
+#define _FLUSH
+#ifdef _FLUSH
+extern void __ssb_pso_feedinput(uint32_t vector[], size_t size);
+#endif
+
 __attribute__((no_softstorebuffer))
 int main(int argc, char *argv[])
 {
+#ifdef _FLUSH
+	// Suppose a fuzzer provides an input that do not flush arg->ptr
+	// but flush arg->ready.
+	uint32_t input[2] = { 0, 1 };
+	__ssb_pso_feedinput(input, 2);
+#endif
 	pthread_t pth1, pth2;
 	struct shared_t shared = { 0, 0 };
 
