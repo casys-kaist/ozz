66bdf87232dd14a8b277f2ee1532fb11057ab8cc
Dae R. Jeong <threeearcat@gmail.com>
2021-02-27 10:32:07 +0000

    scripts/linux/select_arch.sh: DO NOT EXPORT $CONFIG BY DEFAULT

--- scripts/linux/select_arch.sh
+++ scripts/linux/select_arch.sh
@@ -3,12 +3,10 @@
 if [ "$1" = "aarch64" -o "$1" = "arm64" ]; then
 	export ARCH="arm64"
 	export CROSS_COMPILE="aarch64-linux-gnu-"
-	export CONFIG="$PROJECT_HOME/kernels/guest/configs/config.aarch64"
 	MEMMODEL="PSO"
 elif [ "$1" = "x86_64" ]; then
 	# Well, we are not cross-compiling
 	export ARCH="x86_64"
-	export CONFIG="$PROJECT_HOME/kernels/guest/configs/config.x86_64"
 	MEMMODEL="TSO"
 else 
 	echo "Unknown arch"
