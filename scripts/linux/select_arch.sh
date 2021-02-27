56dfe3829a8f49d6ad30258a34a694ce9f264ece
Dae R. Jeong <threeearcat@gmail.com>
2021-02-27 07:43:20 +0000

    scripts/linux/select_arch.sh: export KCFLAGS to instrument a kernel if INSTRUMENT=1

--- scripts/linux/select_arch.sh
+++ scripts/linux/select_arch.sh
@@ -1,16 +1,22 @@
 #!/bin/sh -e
 
-export LLVM=1
-
 if [ "$1" = "aarch64" -o "$1" = "arm64" ]; then
 	export ARCH="arm64"
 	export CROSS_COMPILE="aarch64-linux-gnu-"
 	export CONFIG="$PROJECT_HOME/kernels/guest/configs/config.aarch64"
+	MEMMODEL="PSO"
 elif [ "$1" = "x86_64" ]; then
 	# Well, we are not cross-compiling
+	export ARCH="x86_64"
 	export CONFIG="$PROJECT_HOME/kernels/guest/configs/config.x86_64"
+	MEMMODEL="TSO"
 else 
 	echo "Unknown arch"
 	return 1
 fi
 
+export LLVM=1
+if [ -n "$INSTRUMENT" ]; then
+	PASS="$TOOLS_DIR/SoftStoreBufferPass/build/pass/libSSBPass.so"
+	export KCFLAGS="-Xclang -load -Xclang $PASS -mllvm -arch=$ARCH -mllvm -memorymodel=$MEMMODEL"
+fi
