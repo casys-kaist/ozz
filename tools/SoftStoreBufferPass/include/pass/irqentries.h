5e86f390f74ecf33995ecb405460f732d0435aa3
Dae R. Jeong <threeearcat@gmail.com>
2021-03-01 08:30:42 +0000

    SoftStoreBuffer/pass: extract_entry.sh
    In some architectures (e.g., x86_64), we can hardly identify a
    function is an irq entry with the given function definition. Let's
    parse a source file and extract the list of irq entries so we can
    easily identify them.

---
+++ tools/SoftStoreBufferPass/include/pass/irqentries.h
@@ -0,0 +1,8 @@
+#ifndef __IRQENTRIES_H
+#define __IRQENTRIES_H
+
+std::string IRQEntriesX86_64[] = {
+#include "irqentries_x86_64.inc"
+};
+
+#endif // __IRQENTRIES_H
---
+++ tools/SoftStoreBufferPass/scripts/extract_entry.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+# In some architectures (e.g., x86_64), we can hardly identify a
+# function is an irq entry with the given function definition. Let's
+# parse a source file and extract the list of irq entries so we can
+# easily identify them.
+
+LINUX="$PROJECT_HOME/kernels/guest/linux"
+
+extract_x86_64_idtentries() {
+	X86_IDTENTRY_H="$LINUX/arch/x86/include/asm/idtentry.h"
+
+	REGEXP="^DECLARE_IDTENTRY.*,[[:space:]]*([^\)]*).*\);"
+	SEDCMD="s/$REGEXP/\"\1\",/p"
+
+	DST_X86="$TOOLS_DIR/SoftStoreBufferPass/include/pass/irqentries_x86_64.inc"
+
+	sed -nE "$SEDCMD" "$X86_IDTENTRY_H" > "$DST_X86"
+}
+
+extract_x86_64_idtentries
