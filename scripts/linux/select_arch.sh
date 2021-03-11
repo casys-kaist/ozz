b05e46641d7c07c10e8a5146026270abdc0cd9b5
Dae R. Jeong <threeearcat@gmail.com>
2021-03-11 03:21:01 +0000

    scripts/linux/select_arch.sh: minor changes
    1) Always select the PSO memory model. We don't want to run TSO
    2) Define the INSTRUMENT_KSSB macro to let the kernel know that we are
    instrumenting.

--- scripts/linux/select_arch.sh
+++ scripts/linux/select_arch.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -e
 
+# XXX: This seems a bit weird. No matter the architecture is, we
+# emulate the PSO memory model. All we want is to cross compile the
+# kernel for aarch64, and x86_64 is used to only for debugging and
+# testing.
+MEMMODEL="PSO"
+
 if [ "$1" = "aarch64" -o "$1" = "arm64" ]; then
 	export ARCH="arm64"
 	export CROSS_COMPILE="aarch64-linux-gnu-"
-	MEMMODEL="PSO"
 elif [ "$1" = "x86_64" ]; then
 	# Well, we are not cross-compiling
 	export ARCH="x86_64"
-	MEMMODEL="TSO"
 else 
 	echo "Unknown arch"
 	return 1
@@ -16,5 +20,5 @@ fi
 export LLVM=1
 if [ -n "$INSTRUMENT" ]; then
 	PASS="$TOOLS_DIR/SoftStoreBufferPass/build/pass/libSSBPass.so"
-	export KCFLAGS="-Xclang -load -Xclang $PASS -mllvm -arch=$ARCH -mllvm -memorymodel=$MEMMODEL"
+	export KCFLAGS="-Xclang -load -Xclang $PASS -mllvm -arch=$ARCH -mllvm -memorymodel=$MEMMODEL -DINSTRUMENT_KSSB"
 fi
