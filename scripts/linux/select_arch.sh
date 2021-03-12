2f78df449cdbe1e1f4f09ba784019c74ff09a4d1
Dae R. Jeong <threeearcat@gmail.com>
2021-03-12 04:36:04 +0000

    scripts/linux/select_arch.sh: unser CROSS_COMPILE when selecting x86_64

--- scripts/linux/select_arch.sh
+++ scripts/linux/select_arch.sh
@@ -10,8 +10,9 @@ if [ "$1" = "aarch64" -o "$1" = "arm64" ]; then
 	export ARCH="arm64"
 	export CROSS_COMPILE="aarch64-linux-gnu-"
 elif [ "$1" = "x86_64" ]; then
-	# Well, we are not cross-compiling
 	export ARCH="x86_64"
+	# Well, we are not cross-compiling
+	export CROSS_COMPILE=
 else 
 	echo "Unknown arch"
 	return 1
