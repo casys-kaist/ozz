4da2584515d81443bc01c7d650cbbbccc4d52025
Dae R. Jeong <threeearcat@gmail.com>
2021-03-15 12:23:36 +0000

    scripts/go: add $GOTOOLS_DIR/bin to PATH

--- scripts/go/_envsetup.sh
+++ scripts/go/_envsetup.sh
@@ -3,6 +3,7 @@
 # Be aware of conflictions with GO's environment variables
 __export_envvar "GO" "$TOOLCHAINS_DIR/go"
 __append_path "$GO_PATH/bin"
+__append_path "$GOTOOLS_DIR/bin"
 export GO="$GOROOT/bin/go"
 export GOROOT="$GO_PATH"
 export GOPATH="$GOTOOLS_DIR"
