8ac27e2ffa37c6aa142543c881bd38e2d5ffb86b
Dae R. Jeong <threeearcat@gmail.com>
2021-07-05 07:49:56 +0000

    kernels/guest/modules/trampoline: call a hypercall to nodify the address of trampoline
    Minor: prepare to add other modules

--- kernels/guest/modules/trampoline/trampoline.c
+++ kernels/guest/modules/trampoline/trampoline.c
@@ -1,6 +1,10 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+
+#include "hcall.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dae R. Jeong");
@@ -22,12 +26,15 @@ loop:
 EXPORT_SYMBOL(trampoline);
 
 static int __init trampoline_init(void) {
-	printk(KERN_INFO "Installing trampoline\n");
+	unsigned long addr = (unsigned long)trampoline;
+	pr_info("Installing trampoline\n");
+	pr_info("Trampoline addr: %lx\n", addr);
+	hypercall(HCALL_VMI_FUNC_ADDR, VMI_TRAMPOLINE, addr, 0);
 	return 0;
 }
 
 static void __exit trampoline_exit(void) {
-	printk(KERN_INFO "Uninstalling trampoline\n");
+	pr_info("Uninstalling trampoline\n");
 }
 
 module_init(trampoline_init);
