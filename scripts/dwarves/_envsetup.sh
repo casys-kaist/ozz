d51e405fbc265aa4a54cbd1a381f10ced2531c0e
Dae R. Jeong <threeearcat@gmail.com>
2021-12-02 07:28:54 +0000

    scripts/dwarves: handle empty $LD_LIBRARY_PATH
    This broke the installation of buildroot

--- scripts/dwarves/_envsetup.sh
+++ scripts/dwarves/_envsetup.sh
@@ -2,6 +2,10 @@
 
 __export_envvar "DWARVES" "$TOOLCHAINS_DIR/dwarves"
 __append_path "$DWARVES_INSTALL/bin"
-export LD_LIBRARY_PATH="$DWARVES_INSTALL/lib":$LD_LIBRARY_PATH
+if [ -z "$LD_LIBRARY_PATH" ]; then
+	export LD_LIBRARY_PATH="$DWARVES_INSTALL/lib"
+else
+	export LD_LIBRARY_PATH="$DWARVES_INSTALL/lib":$LD_LIBRARY_PATH
+fi
 export DWARVES_VERSION="v1.19"
 
