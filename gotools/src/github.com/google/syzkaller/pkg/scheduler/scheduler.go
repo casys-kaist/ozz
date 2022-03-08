b2cb698fe5383fbde8a9d7be1a5f9f5c9a8a7e55
Dae R. Jeong <threeearcat@gmail.com>
2022-03-08 07:00:00 +0000

    pkg/scheduler: move orchestrator code into a dedicated file
    TODO: orchestrator contains the O(n*n) time complexity algorithm. We
    can possibly improve the time complexity of
    orchestrator.harmoniousKnot().

--- gotools/src/github.com/google/syzkaller/pkg/scheduler/scheduler.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/scheduler.go
@@ -313,38 +313,6 @@ func (knotter *Knotter) GetKnots() []primitive.Segment {
 	return knotter.knots
 }
 
-type orchestrator struct {
-	// Communications that are already selected
-	comms []primitive.Communication
-	// Input knots
-	knots []primitive.Knot
-}
-
-func (orch *orchestrator) selectHarmoniousKnots() []primitive.Knot {
-	res := []primitive.Knot{}
-	remaining := make([]primitive.Knot, 0, len(orch.knots))
-	for _, knot := range orch.knots {
-		if orch.harmoniousKnot(knot) {
-			res = append(res, knot)
-			orch.comms = append(orch.comms, knot[0], knot[1])
-		} else {
-			remaining = append(remaining, knot)
-		}
-	}
-	orch.knots = remaining
-	orch.comms = nil
-	return res
-}
-
-func (orch orchestrator) harmoniousKnot(knot primitive.Knot) bool {
-	for _, comm := range orch.comms {
-		if knot[0].Conflict(comm) || knot[1].Conflict(comm) {
-			return false
-		}
-	}
-	return true
-}
-
 type Scheduler struct {
 	// input
 	Knots []primitive.Knot
--- gotools/src/github.com/google/syzkaller/pkg/scheduler/scheduler_test.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/scheduler_test.go
@@ -113,33 +113,6 @@ func testExcavateKnots(t *testing.T, filename string, answer primitive.Knot) []p
 	return knots
 }
 
-func TestSelectHarmoniousKnotsIterSimple(t *testing.T) {
-	for _, test := range testsSingleSeq {
-		testSelectHarmoniousKnotsIter(t, test.filename, test.answer)
-	}
-}
-
-func testSelectHarmoniousKnotsIter(t *testing.T, path string, answer primitive.Knot) {
-	knots := loadKnots(t, []string{path})
-
-	orch := orchestrator{knots: knots}
-	i, count := 0, 0
-	for len(orch.knots) != 0 {
-		selected := orch.selectHarmoniousKnots()
-		count += len(selected)
-		t.Logf("Selected:")
-		found := checkAnswer(t, selected, answer)
-		if found {
-			t.Logf("Found: %d", i)
-		}
-		i++
-	}
-
-	if count != len(knots) {
-		t.Errorf("wrong number of selected knots, expected %v, got %v", len(knots), count)
-	}
-}
-
 func TestGenerateSchedPoint(t *testing.T) {
 	knots := loadKnots(t, []string{"data1_simple"})
 
