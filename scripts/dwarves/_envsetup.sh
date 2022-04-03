5556134739ba89809d5557c98a4a2cd9e90689ca
Dae R. Jeong <threeearcat@gmail.com>
2022-04-03 11:51:55 +0000

    scripts/capstone: scripts for installing capstone
    minor: also update the dwarves script

--- scripts/dwarves/_envsetup.sh
+++ scripts/dwarves/_envsetup.sh
@@ -2,10 +2,6 @@
 
 __export_envvar "DWARVES" "$TOOLCHAINS_DIR/dwarves"
 __append_path "$DWARVES_INSTALL/bin"
-if [ -z "$LD_LIBRARY_PATH" ]; then
-	export LD_LIBRARY_PATH="$DWARVES_INSTALL/lib"
-else
-	export LD_LIBRARY_PATH="$DWARVES_INSTALL/lib":$LD_LIBRARY_PATH
-fi
+export LD_LIBRARY_PATH="$DWARVES_INSTALL/lib"${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 export DWARVES_VERSION="v1.19"
 
