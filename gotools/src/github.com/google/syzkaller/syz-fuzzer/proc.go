8a1515921737be0d9c58c2132a780155aa1555fa
Dae R. Jeong <threeearcat@gmail.com>
2022-06-13 06:18:25 +0000

    syzkaller/syz-fuzzer: increase the number of intermediate calls

--- gotools/src/github.com/google/syzkaller/syz-fuzzer/proc.go
+++ gotools/src/github.com/google/syzkaller/syz-fuzzer/proc.go
@@ -492,7 +492,7 @@ func (proc *Proc) pickupThreadingWorks(p *prog.Prog, info *ipc.ProgInfo) {
 		return
 	}
 
-	maxIntermediateCalls := 2
+	maxIntermediateCalls := 3
 	intermediateCalls := func(c1, c2 int) int {
 		return c2 - c1 - 1
 	}
