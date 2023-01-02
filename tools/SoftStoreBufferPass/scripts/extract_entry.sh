f2dadd8dbd022c7bb29c699216d87b235e746665
Dae R. Jeong <threeearcat@gmail.com>
2023-01-02 06:47:04 +0000

    SSBpass: update irq entries for x86_64

--- tools/SoftStoreBufferPass/scripts/extract_entry.sh
+++ tools/SoftStoreBufferPass/scripts/extract_entry.sh
@@ -5,7 +5,7 @@
 # parse a source file and extract the list of irq entries so we can
 # easily identify them.
 
-LINUX="$PROJECT_HOME/kernels/guest/linux"
+LINUX="$PROJECT_HOME/kernels/linux"
 
 extract_x86_64_idtentries() {
 	X86_IDTENTRY_H="$LINUX/arch/x86/include/asm/idtentry.h"
