75fef7b837c271d32e5d34e1b49c8f02de67ef7f
Dae R. Jeong <threeearcat@gmail.com>
2023-02-20 06:33:39 +0000

    Revert "scripts/qemu: gdb: use default port"
    This reverts commit 14bc95a4610aaa96614731a4c2ec25a707acdc1b.
    
    still breakpoint does not work correctly

--- scripts/gdb/attach_gdb.sh
+++ scripts/gdb/attach_gdb.sh
@@ -5,13 +5,13 @@
 BATCHCMD_DEFAULT=$TMP_DIR/gdb-cmds.batch
 
 if [ -z "$ARCH" ]; then
-    ARCH="x86_64"
+	ARCH="x86_64"
 fi
 
 if [ "$ARCH" = "x86_64" ]; then
-    TARGET_ARCH="i386:x86-64:intel"
+	TARGET_ARCH="i386:x86-64:intel"
 else
-    TARGET_ARCH="aarch64"
+	TARGET_ARCH="aarch64"
 fi
 
 _UID=$(id -u)
@@ -19,21 +19,21 @@ GDBPORT=$(echo "1234 + $_UID" | bc -l)
 
 cat <<EOF > $BATCHCMD_DEFAULT
 set architecture $TARGET_ARCH
-target remote :1234
+target remote :$GDBPORT
 set disassemble-next-line on
 EOF
 
 if [ "$#" -lt "1" ]; then
-    echo "[WARN] Missing a vmlinux path"
-    echo "[WARN] Trying \"kernels/guest/builds/$ARCH/vmlinux\""
-    echo
-    VMLINUX="$PROJECT_HOME/kernels/guest/builds/$ARCH/vmlinux"
+	echo "[WARN] Missing a vmlinux path"
+	echo "[WARN] Trying \"kernels/guest/builds/$ARCH/vmlinux\""
+	echo
+	VMLINUX="$PROJECT_HOME/kernels/guest/builds/$ARCH/vmlinux"
 else
-    VMLINUX=$1
+	VMLINUX=$1
 fi
 
 if [ -n "$BATCHCMD" ]; then
-    BATCHCMD_ADDITIONAL="-x $BATCHCMD"
+	BATCHCMD_ADDITIONAL="-x $BATCHCMD"
 fi
 
 
