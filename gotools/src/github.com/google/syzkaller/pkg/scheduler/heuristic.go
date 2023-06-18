29ff5976921023ae8306c4a92c3119a7df08a2d2
Dae R. Jeong <threeearcat@gmail.com>
2023-06-18 13:13:39 +0000

    syzkaller/pkg/scheduler: implement ComputePotentialBuggyKnots

--- gotools/src/github.com/google/syzkaller/pkg/scheduler/heuristic.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/heuristic.go
@@ -1,8 +1,39 @@
 package scheduler
 
-import "github.com/google/syzkaller/pkg/interleaving"
+import (
+	"github.com/google/syzkaller/pkg/interleaving"
+)
 
-type chunk []interleaving.Access
+type chunk interleaving.SerialAccess
+
+// TODO: I think we can implement this using more of the previous
+// implementation
+func ComputePotentialBuggyKnots(seq []interleaving.SerialAccess) []interleaving.Segment {
+	if len(seq) != 2 {
+		return nil
+	}
+
+	cs0, cs1 := chunkize(seq[0]), chunkize(seq[1])
+	// TODO: optimize
+	knots := []interleaving.Segment{}
+	for _, c0 := range cs0 {
+		for _, c1 := range cs1 {
+			knots = append(knots, computePotentialBuggyKnots(c0, c1)...)
+		}
+	}
+	// TODO: optimize
+	ht := make(map[uint64]struct{})
+	res := []interleaving.Segment{}
+	for _, knot := range knots {
+		hsh := knot.Hash()
+		if _, ok := ht[hsh]; ok {
+			continue
+		}
+		ht[hsh] = struct{}{}
+		res = append(res, knot)
+	}
+	return res
+}
 
 func chunkize(serial interleaving.SerialAccess) []chunk {
 	chunks := []chunk{}
@@ -28,4 +59,22 @@ func chunkize(serial interleaving.SerialAccess) []chunk {
 		}
 	}
 	return chunks
+}
+
+func computePotentialBuggyKnots(c0, c1 chunk) []interleaving.Segment {
+	knotter := GetKnotter(opts)
+	knotter.AddSequentialTrace(
+		[]interleaving.SerialAccess{
+			interleaving.SerializeAccess(c0),
+			interleaving.SerializeAccess(c1),
+		})
+	knotter.ExcavateKnots()
+	return knotter.GetKnots()
+}
+
+var opts = KnotterOpts{
+	Flags: FlagWantMessagePassing |
+		FlagWantParallel |
+		FlagDifferentAccessTypeOnly |
+		FlagWantStrictMessagePassing,
 }
--- gotools/src/github.com/google/syzkaller/pkg/scheduler/heuristic_test.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/heuristic_test.go
@@ -1,6 +1,10 @@
 package scheduler
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/syzkaller/pkg/interleaving"
+)
 
 func TestChunknize(t *testing.T) {
 	seq := loadTestdata(t, []string{"pso_test"}, nil)[0]
@@ -14,5 +18,30 @@ func TestChunknize(t *testing.T) {
 				t.Logf("%v", acc)
 			}
 		}
+	}
+}
+
+func TestComputePotentialBuggyKnots(t *testing.T) {
+	filename := "pso_test"
+	ans := interleaving.Knot{
+		{{Inst: 0x81a6167c, Size: 4, Typ: interleaving.TypeStore, Timestamp: 6}, {Inst: 0x81a61ba4, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 1750}},
+		{{Inst: 0x81a616a6, Size: 4, Typ: interleaving.TypeStore, Timestamp: 8}, {Inst: 0x81a61af7, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 1749}}}
+
+	seq := loadTestdata(t, []string{filename}, nil)[0]
+	res := ComputePotentialBuggyKnots(seq[:])
+	knots := []interleaving.Knot{}
+	for _, knot0 := range res {
+		knots = append(knots, knot0.(interleaving.Knot))
+	}
+
+	for i, knot0 := range res {
+		knot := knot0.(interleaving.Knot)
+		t.Logf("%d-th knot (%v)", i, knot.Hash())
+		for _, comm := range knot {
+			t.Logf("%v --> %v", comm.Former(), comm.Latter())
+		}
+	}
+	if !checkAnswer(t, knots, ans) {
+		t.Errorf("%s: can't find the required knot", filename)
 	}
 }
