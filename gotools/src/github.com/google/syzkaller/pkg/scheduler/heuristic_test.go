1d3e1250764dbd08d745f835dd65362d11804471
Dae R. Jeong <threeearcat@gmail.com>
2023-06-26 11:59:07 +0000

    syzkaller/pkg/scheduler: test for watchqueue_pipe

--- gotools/src/github.com/google/syzkaller/pkg/scheduler/heuristic_test.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/heuristic_test.go
@@ -22,26 +22,41 @@ func TestChunknize(t *testing.T) {
 }
 
 func TestComputePotentialBuggyKnots(t *testing.T) {
-	filename := "pso_test"
-	ans := interleaving.Knot{
-		{{Inst: 0x81a6167c, Size: 4, Typ: interleaving.TypeStore, Timestamp: 6}, {Inst: 0x81a61ba4, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 1750}},
-		{{Inst: 0x81a616a6, Size: 4, Typ: interleaving.TypeStore, Timestamp: 8}, {Inst: 0x81a61af7, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 1749}}}
-
-	seq := loadTestdata(t, []string{filename}, nil)[0]
-	res := ComputePotentialBuggyKnots(seq[:])
-	knots := []interleaving.Knot{}
-	for _, knot0 := range res {
-		knots = append(knots, knot0.(interleaving.Knot))
+	tests := []struct {
+		filename string
+		ans      interleaving.Knot
+	}{
+		{
+			filename: "pso_test",
+			ans: interleaving.Knot{
+				{{Inst: 0x81a6167c, Size: 4, Typ: interleaving.TypeStore, Timestamp: 6}, {Inst: 0x81a61ba4, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 1750}},
+				{{Inst: 0x81a616a6, Size: 4, Typ: interleaving.TypeStore, Timestamp: 8}, {Inst: 0x81a61af7, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 1749}}},
+		},
+		{
+			filename: "watchqueue_pipe",
+			ans: interleaving.Knot{
+				{{Inst: 0x81ad9a0c, Size: 8, Typ: interleaving.TypeStore, Timestamp: 98}, {Inst: 0x81f83178, Size: 8, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 197}},
+				{{Inst: 0x81ad9a84, Size: 4, Typ: interleaving.TypeStore, Timestamp: 102}, {Inst: 0x81a61af7, Size: 4, Typ: interleaving.TypeLoad, Thread: 1, Timestamp: 191}}},
+		},
 	}
 
-	for i, knot0 := range res {
-		knot := knot0.(interleaving.Knot)
-		t.Logf("%d-th knot (%v)", i, knot.Hash())
-		for _, comm := range knot {
-			t.Logf("%v --> %v", comm.Former(), comm.Latter())
+	for _, test := range tests {
+		seq := loadTestdata(t, []string{test.filename}, nil)[0]
+		res := ComputePotentialBuggyKnots(seq[:])
+		knots := []interleaving.Knot{}
+		for _, knot0 := range res {
+			knots = append(knots, knot0.(interleaving.Knot))
 		}
-	}
-	if !checkAnswer(t, knots, ans) {
-		t.Errorf("%s: can't find the required knot", filename)
+
+		for i, knot0 := range res {
+			knot := knot0.(interleaving.Knot)
+			t.Logf("%d-th knot (%v)", i, knot.Hash())
+			for _, comm := range knot {
+				t.Logf("%v --> %v", comm.Former(), comm.Latter())
+			}
+		}
+		if !checkAnswer(t, knots, test.ans) {
+			t.Errorf("%s: can't find the required knot", test.filename)
+		}
 	}
 }
