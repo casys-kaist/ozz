87d4865bcbe9b76812a29cd09f6a000862c5b29b
Dae R. Jeong <threeearcat@gmail.com>
2023-07-06 01:10:30 +0000

    scripts/llvm: enable shared libs to reduce the build size
    Minor change: add $LLVM_INSTALL/lib into LD_LIBRARY_PATH as now shared
    libs are not built into binaries

--- scripts/llvm/_envsetup.sh
+++ scripts/llvm/_envsetup.sh
@@ -2,5 +2,6 @@
 
 __export_envvar "LLVM" "$TOOLCHAINS_DIR/llvm"
 __append_path "$LLVM_INSTALL/bin"
+export LD_LIBRARY_PATH="$LLVM_INSTALL/lib"${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 export CLANG="$LLVM_INSTALL/bin/clang"
 export LLVM_VERSION="llvmorg-12.0.1"
