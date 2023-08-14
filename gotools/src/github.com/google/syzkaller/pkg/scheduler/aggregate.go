fc0f91191596e4ecf0b15b34aa24e8c05e261e21
Dae R. Jeong <threeearcat@gmail.com>
2023-08-14 07:19:02 +0000

    syzkaller/pkg/scheduler: implement the aggregation algorithm

--- gotools/src/github.com/google/syzkaller/pkg/scheduler/aggregate.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/aggregate.go
@@ -9,14 +9,13 @@ import (
 func GenerateCandidates(rnd *rand.Rand, hints []interleaving.Segment) (interleaving.Candidate, []interleaving.Segment, []interleaving.Segment) {
 	idx := rnd.Intn(len(hints))
 	pivot := hints[idx].(interleaving.Knot)
-	// TODO: Why is this always a crit comm?
-	critComm := pivot[1]
+	critComm := extractCriticalCommunication(pivot)
 	return aggregateCandidates(critComm, hints)
 }
 
 func aggregateCandidates(critComm interleaving.Communication, hints []interleaving.Segment) (interleaving.Candidate, []interleaving.Segment, []interleaving.Segment) {
 	collected, remaining := collectSegments(critComm, hints)
-	cand := interleaving.Candidate{}
+	cand := constructCandidate(critComm, collected)
 	return cand, collected, remaining
 }
 
@@ -24,32 +23,37 @@ func collectSegments(critComm interleaving.Communication, hints []interleaving.S
 	critHash := critComm.Hash()
 	for _, seg := range hints {
 		knot := seg.(interleaving.Knot)
-		crits := extractCriticalCommunication(knot)
-		for _, crit := range crits {
-			if hsh := crit.Hash(); hsh == critHash {
-				collected = append(collected, seg)
-			} else {
-				remaining = append(remaining, seg)
-			}
+		crit := extractCriticalCommunication(knot)
+		if hsh := crit.Hash(); hsh == critHash {
+			collected = append(collected, seg)
+		} else {
+			remaining = append(remaining, seg)
 		}
 	}
 	return
 }
 
-func extractCriticalCommunication(knot interleaving.Knot) []interleaving.Communication {
-	if isMessagePassing(knot[0], knot[1]) {
-		return []interleaving.Communication{knot[1]}
-	} else {
-		return []interleaving.Communication{knot[0], knot[1]}
-	}
+func extractCriticalCommunication(knot interleaving.Knot) interleaving.Communication {
+	// NOTE: knot[1] is always a criticall communication
+	return knot[1]
 }
 
-func constructCandidate(segs []interleaving.Segment) interleaving.Candidate {
+func constructCandidate(critComm interleaving.Communication, segs []interleaving.Segment) interleaving.Candidate {
+	// TODO: The reason why I name variables some* is that we don't
+	// sufficiently understand characteristics/nature of such bugs,
+	// someComm, and someInst. When we understand that, rename thus
+	// variables.
+	someInst := []interleaving.Access{}
 	for _, seg := range segs {
-		_, ok := seg.(interleaving.Knot)
+		knot, ok := seg.(interleaving.Knot)
 		if !ok {
 			panic("wrong")
 		}
+		someComm := knot[0]
+		someInst = append(someInst, someComm.Former())
 	}
-	return interleaving.Candidate{}
+	return interleaving.Candidate{
+		DelayingInst: someInst,
+		CriticalComm: critComm,
+	}
 }
