3cc93ef356992064e2fe71f5f5e708e4761811c7
Dae R. Jeong <threeearcat@gmail.com>
2023-08-19 12:01:57 +0000

    syzkaller/syz-fuzzer: add printSeq() used for debugging

--- gotools/src/github.com/google/syzkaller/syz-fuzzer/debug.go
+++ gotools/src/github.com/google/syzkaller/syz-fuzzer/debug.go
@@ -31,4 +31,13 @@ func testingHints(hint []interleaving.Segment) bool {
 	return false
 }
 
+func printSeq(seq []interleaving.SerialAccess) {
+	for i, serial := range seq {
+		log.Logf(0, "%d-th", i)
+		for _, acc := range serial {
+			log.Logf(0, "%v", acc)
+		}
+	}
+}
+
 var _debug bool = true
