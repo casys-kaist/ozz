4cea8a277e0965891ef6d84013eeda0e8e950a90
Dae R. Jeong <threeearcat@gmail.com>
2023-08-26 09:38:29 +0000

    scripts/linux/build_setup.sh: do not instrument dynamic allocas
    Our FuncEntry callbacks break the entry basic block. As a result, some
    static allocas turns out dynamic allocas which causes the KASAN
    instrumentation for dynamic allocas. Not sure why, but this breaks the
    correctness related to put_user_*(), specifically related to the ebx
    clobber.
    
    As it is unlikely that interesting bugs cause
    stack-out-of-bounds, (perhaps) we can safely disable
    -asan-instrument-dynamic-allocas.

--- scripts/linux/build_setup.sh
+++ scripts/linux/build_setup.sh
@@ -6,8 +6,8 @@ export ARCH="x86_64"
 export LLVM=1
 
 PASS="$TOOLS_DIR/SoftStoreBufferPass/build/pass/libSSBPass.so"
-export CFLAGS_KSSB="-Xclang -load -Xclang $PASS -mllvm -arch=$ARCH -mllvm -memorymodel=$MEMMODEL"
-export CFLAGS_KSSB_FLUSHONLY="-Xclang -load -Xclang $PASS -mllvm -arch=$ARCH -mllvm -memorymodel=$MEMMODEL -mllvm -ssb-flush-only=true"
+export CFLAGS_KSSB="-Xclang -load -Xclang $PASS -mllvm -arch=$ARCH -mllvm -memorymodel=$MEMMODEL -mllvm -asan-instrument-dynamic-allocas=false"
+export CFLAGS_KSSB_FLUSHONLY="$CFLAGS_KSSB -mllvm -ssb-flush-only=true"
 if [ -n "$FIRSTPASS" ]; then
 	export CFLAGS_KSSB="$CFLAGS_KSSB -mllvm -ssb-second-pass=false"
 	export _FIRSTPASS=1
