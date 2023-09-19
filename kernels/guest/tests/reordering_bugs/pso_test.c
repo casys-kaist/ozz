ad0cad33caa94d5eb644bec286dc6b3c3ba4efbb
Dae R. Jeong <threeearcat@gmail.com>
2023-09-19 10:54:23 +0000

    kernels/linux: update the guest kernel

--- kernels/guest/tests/reordering_bugs/pso_test.c
+++ kernels/guest/tests/reordering_bugs/pso_test.c
@@ -15,7 +15,7 @@
 
 void *th1(void *_arg) {
   pin(1);
-  hypercall(HCALL_INSTALL_BP, 0xffffffff81bac3e7, 0, 0);
+  hypercall(HCALL_INSTALL_BP, 0xffffffff81bd2cf7, 0, 0);
   activate_bp_sync();
   syscall(SYS_SSB_SWITCH);
   syscall(SYS_PSO_WRITER, 0, 0, 0);
