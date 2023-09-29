991c3026fe77c0d342b7c39a83545b4b32d2f82e
Dae R. Jeong <threeearcat@gmail.com>
2023-09-29 03:31:42 +0000

    scripts/exp/scrap_crash.py: record kernel info

--- scripts/exp/scrap_crash.py
+++ scripts/exp/scrap_crash.py
@@ -5,7 +5,31 @@ import shutil
 import sys
 
 
-def scrap_crash(crash, report_dir):
+def get_kernel_info(exp_dir, kernels_dir):
+    try:
+        with open(os.path.join(exp_dir, "x86_64", "workdir", "running")) as f:
+            kernel_hsh = f.read().strip()
+    except:
+        kernel_hsh = "unknown"
+
+    try:
+        with open(os.path.join(kernels_dir, "guest", "BUILD_HISTORY")) as f:
+            build_history = f.readlines()
+            assert len(build_history) > 0
+    except:
+        build_history = ["unknown"]
+
+    kernel_info = ""
+    kernel_info += build_history[0]
+    for h in build_history[1:]:
+        if h.find(kernel_hsh):
+            kernel_info += h
+    return kernel_info
+
+
+def scrap_crash(crash, exp_dir, kernels_dir):
+    report_dir = os.path.join(exp_dir, "report")
+
     crash_hash = os.path.basename(crash)
     outdir = os.path.join(report_dir, crash_hash)
     os.makedirs(outdir)
@@ -17,12 +41,19 @@ def scrap_crash(crash, report_dir):
             dst = os.path.join(outdir, file)
             shutil.copyfile(src, dst)
 
+    kernel_info = get_kernel_info(exp_dir, kernels_dir)
+    try:
+        with open(os.path.join(outdir, "kernel_info"), "w") as f:
+            f.write(kernel_info)
+    except:
+        pass
+
 
 def scrap_crashes(crashes):
     exp_dir = os.environ["EXP_DIR"]
-    report_dir = os.path.join(exp_dir, "report")
+    kernels_dir = os.environ["KERNELS_DIR"]
     for crash in crashes:
-        scrap_crash(crash, report_dir)
+        scrap_crash(crash, exp_dir, kernels_dir)
 
 
 def main():
