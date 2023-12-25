bb622fd5f7cc585da16499e1c5afe811d91306d8
Dae R. Jeong <threeearcat@gmail.com>
2023-12-25 12:21:10 +0000

    syzkaller/pkg/scheduler: Implement TestInSameChunk

--- gotools/src/github.com/google/syzkaller/pkg/scheduler/scheduler_test.go
+++ gotools/src/github.com/google/syzkaller/pkg/scheduler/scheduler_test.go
@@ -1,6 +1,11 @@
 package scheduler
 
 import (
+	"bytes"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/google/syzkaller/pkg/interleaving"
@@ -43,6 +48,96 @@ func TestLockContending(t *testing.T) {
 			t.Errorf("Wrong, ans: %d, got: %d", test.num, numComms)
 		}
 	}
+}
+
+type singleInSameChunkTest struct {
+	acc     [2]interleaving.Access
+	store   bool
+	allowed bool
+}
+
+func TestInSameChunk(t *testing.T) { //
+	tests := []struct {
+		fn   string
+		test []singleInSameChunkTest
+	}{
+		{
+			fn: "watchqueue",
+			test: []singleInSameChunkTest{
+				{acc: [2]interleaving.Access{
+					{0x81c6f5fa, 0xb5cf020, 8, 0, 63084, 0},
+					{0x81c6f69d, 0x7b92930, 4, 0, 63089, 0}},
+					store:   true,
+					allowed: true},
+				{acc: [2]interleaving.Access{
+					{0x81c6f56a, 0x253c357c, 4, 0, 63077, 0},
+					{0x81c6f69d, 0x7b92930, 4, 0, 63089, 0}},
+					store:   true,
+					allowed: false},
+			},
+		},
+	}
+	for _, test := range tests {
+		seq := loadTestdata(t, test.fn)
+		printSeq(t, seq)
+		knotter := Knotter{}
+		knotter.loopAllowed = loopAllowed
+		knotter.AddSequentialTrace(seq)
+		knotter.collectCommChans()
+		knotter.buildAccessMap()
+		knotter.annotateLocks()
+		knotter.chunknizeSerials()
+		knotter.formCommunications()
+		for _, test0 := range test.test {
+			if res := knotter.inSameChunk(test0.acc[0], test0.acc[1], test0.store); res != test0.allowed {
+				t.Errorf("Wrong, want: %v, got: %v", test0.allowed, res)
+			}
+		}
+	}
+}
+
+func loadTestdata(t *testing.T, fn string) []interleaving.SerialAccess {
+	path := filepath.Join("testdata", fn)
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	res := make([]interleaving.SerialAccess, 2)
+	var tid uint32
+	for {
+		idx := bytes.IndexByte(data, byte('\n'))
+		if idx == -1 {
+			break
+		}
+		line := string(data[:idx])
+		data = data[idx+1:]
+
+		toks := strings.Fields(line)
+		if strings.HasPrefix(line, "serial") {
+			// new
+			tid0, err := strconv.Atoi(toks[1])
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+			tid = uint32(tid0)
+			continue
+		}
+		inst, _ := strconv.ParseUint(toks[0], 0, 64)
+		addr, _ := strconv.ParseUint(toks[1], 0, 64)
+		size, _ := strconv.ParseUint(toks[2], 10, 64)
+		typ, _ := strconv.ParseUint(toks[3], 10, 64)
+		ts, _ := strconv.ParseUint(toks[4], 10, 64)
+		acc := interleaving.Access{
+			Inst:      uint32(inst),
+			Addr:      uint32(addr),
+			Size:      uint32(size),
+			Typ:       uint32(typ),
+			Timestamp: uint32(ts),
+			Thread:    uint64(tid),
+		}
+		res[tid] = append(res[tid], acc)
+	}
+	return res
 }
 
 func printSeq(t *testing.T, seq []interleaving.SerialAccess) {
